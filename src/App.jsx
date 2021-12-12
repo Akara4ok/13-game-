4aa54567acf5426481a6535aac8ace67c6102f75
Akara4ok <vladshpilka86@gmail.com>
2021-12-12 13:04:01 +0200

    wrong rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,11 @@
 import React, { useState, useEffect } from "react";
 import classes from "./App.module.scss";
-import Button from "./Components/Button/Button";
-import Popup from "./Components/Popup/Popup";
-import Message from "./Components/Popup/Message";
-import Dice from "react-dice-roll";
-import { fireEvent } from "@testing-library/react";
-
-let refresh = 0;
-let isRules = false;
-
-function useForceUpdate() {
-  const [value, setValue] = useState(0); // integer state
-  return () => setValue((value) => value + 1); // update the state to force render
-}
-
-function onClose(forceUpdate) {
-  forceUpdate();
-  isRules = false;
-}
-
-function getFacesArray(n, m) {
-  const faces = [];
-  for (let i = n; i <= m; i++) {
-    faces.push(
-      `https://raw.githubusercontent.com/Akara4ok/13-game-/main/src/kubik/${i}.png`
-    );
-  }
-  return faces;
-}
+import Layout from "./Components/Layout/Layout";
 
 function App() {
-  const forceUpdate = useForceUpdate();
   return (
     <div className={classes.container}>
-      <header>
-        <h1>Thirteen</h1>
-      </header>
-      <main>
-        <div id="dices" className={classes.dices}>
-          <div>
-            <Dice faces={getFacesArray(1, 6)} triggers={["click", "q"]} />
-          </div>
-          <div>
-            <Dice faces={getFacesArray(2, 7)} triggers={["click", "q"]} />
-          </div>
-        </div>
-        {isRules ? (
-          <Popup>
-            <Message onClose={() => onClose(forceUpdate)}>
-              <p>В игре Три Мушкетера используется доска 5х5 клеток.</p>{" "}
-              <p>
-                В игре участвует два игрока: Мушкетер (синие фишки) и Гвардеец
-                (красные фишки).
-              </p>
-              <p>Цели игры различны для обоих игроков:</p>
-              <ul>
-                <li>
-                  Гвардейцы (красные фишки) побеждают в игре, если им удается
-                  заставить трех мушкетеров оказаться в одном ряду или в одном
-                  столбце.
-                </li>
-                <li>
-                  Мушкетеры (синие фишки) побеждают в игре, если у них в свою
-                  очередь не остается возможных ходов (если хотя бы у одного из
-                  мушкетеров есть ход, игра продолжается).
-                </li>
-              </ul>
-              <p>
-                Игроки ходят по очереди, начиная с того игрока, который играет
-                Мушкетерами.
-              </p>
-              <p>
-                Играющий мушкетерами в свой ход может переместить одного из
-                мушкетеров на соседнюю по горизонтали или вертикали клетку, на
-                которой стоит гвардеец. Гвардеец при этом снимается с доски.
-              </p>
-              <p>
-                Играющий гвардейцами в свой ход может переместить одного из
-                гвардейцев на соседнюю по горизонтали или вертикали свободную
-                клетку.
-              </p>
-            </Message>
-          </Popup>
-        ) : null}
-        <div className={classes.buttonMenu}>
-          <Button
-            onClick={() => {
-              //forceUpdate();
-              //refresh = 1 - refresh;
-            }}
-          >
-            New game
-          </Button>
-          <Button
-            onClick={() => {
-              forceUpdate();
-              isRules = true;
-            }}
-          >
-            Rules
-          </Button>
-        </div>
-      </main>
+      <Layout />
     </div>
   );
 }
---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,273 @@
+import React from "react";
+import classes from "./Layout.module.scss";
+import Button from "../Button/Button";
+import Popup from "../Popup/Popup";
+import Message from "../Popup/Message";
+import Dice from "react-dice-roll";
+
+class Layout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRules: false,
+      isFirstClicked: false,
+      isSecondClicked: false,
+      isBotTurn: false,
+      botsSum: 0,
+      userSum: 0,
+      lastBotSum: 0,
+      currentSum: 0,
+      mul: 1,
+      koef: 1,
+      errorEndTurn: false,
+      userLogs: [],
+      botLogs: [],
+      isNewGame: 0,
+    };
+  }
+
+  componentDidMount() {
+    document.getElementById("1").checked = true;
+    document.getElementById("plus").checked = true;
+  }
+
+  getFacesArray(n, m) {
+    const faces = [];
+    for (let i = n; i <= m; i++) {
+      faces.push(
+        `https://raw.githubusercontent.com/Akara4ok/13-game-/main/src/kubik/${i}.png`
+      );
+    }
+    return faces;
+  }
+
+  endTurnHandler() {
+    const { isFirstClicked, isSecondClicked, mul, koef, userLogs } = this.state;
+    let { currentSum, userSum } = this.state;
+    if (!isFirstClicked || !isSecondClicked) {
+      this.setState({ errorEndTurn: true });
+      return;
+    }
+    this.gamesLog(mul, koef, userSum, currentSum, userLogs);
+    userSum += currentSum * mul * koef;
+    this.setState(
+      {
+        currentSum: 0,
+        isFirstClicked: false,
+        isSecondClicked: false,
+        isBotTurn: true,
+        userSum,
+      },
+      this.botsTurn
+    );
+  }
+
+  onDiceClick(rolled, number) {
+    let { currentSum, isBotTurn } = this.state;
+    currentSum += rolled;
+    if (number === 1 && !isBotTurn) this.setState({ isFirstClicked: true });
+    if (number === 2) {
+      currentSum++;
+      if (!isBotTurn) this.setState({ isSecondClicked: true });
+    }
+    this.setState({ currentSum });
+  }
+
+  botsTurn = () => {
+    document.getElementById("dices").childNodes[0].childNodes[0].click();
+    document.getElementById("dices").childNodes[1].childNodes[0].click();
+    setTimeout(() => {
+      const { currentSum, userSum, botLogs, userLogs } = this.state;
+      let { botsSum } = this.state;
+      this.gamesLog(1, 1, botsSum, currentSum, botLogs);
+      console.log(botLogs);
+      console.log(userLogs);
+      botsSum = botsSum + currentSum;
+      console.log(userSum + " " + botsSum);
+      if(botLogs.length === 5)
+        this.setState({isNewGame: 1});
+      this.setState({
+        isFirstClicked: false,
+        isSecondClicked: false,
+        lastBotSum: currentSum,
+        botsSum,
+        currentSum: 0,
+        isBotTurn: false,
+      });
+    }, 1100);
+  };
+
+  gamesLog(mul, koef, sum, currentSum, logs) {
+    logs.push(`${logs.length + 1}. ${sum} ${koef === 1 ? "+" : '-'}  ${mul}*${currentSum}`);
+  }
+
+  newGame = () => {
+    this.setState({
+      isRules: false,
+      isFirstClicked: false,
+      isSecondClicked: false,
+      isBotTurn: false,
+      botsSum: 0,
+      userSum: 0,
+      lastBotSum: 0,
+      currentSum: 0,
+      mul: 1,
+      koef: 1,
+      errorEndTurn: false,
+      userLogs: [],
+      botLogs: [],
+      isNewGame: 0,
+    });
+  }
+
+  render() {
+    const {
+      isRules,
+      isFirstClicked,
+      isSecondClicked,
+      currentSum,
+      botLogs,
+      userLogs,
+      isBotTurn,
+      lastBotSum,
+      errorEndTurn,
+      botsSum,
+      userSum,
+      isNewGame
+    } = this.state;
+    return (
+      <>
+        <header>
+          <h1>Thirteen</h1>
+        </header>
+        <main>
+          <div className={classes.activityLabel}>
+            <div>Bots activity</div>
+            {botLogs.map(element => <p>{element}</p>)}
+            <div className={classes.scoreWrapper}>Bots Score: {botsSum}</div>
+          </div>
+          <div className={classes.diceButtton}>
+            <div id="dices" className={classes.dices}>
+              <div onClick={() => this.setState({ isFirstClicked: true })}>
+                <Dice
+                  faces={this.getFacesArray(1, 6)}
+                  triggers={["click"]}
+                  onRoll={(rolled) => this.onDiceClick(rolled, 1)}
+                  disabled={isFirstClicked}
+                />
+              </div>
+              <div onClick={() => this.setState({ isSecondClicked: true })}>
+                <Dice
+                  faces={this.getFacesArray(2, 7)}
+                  triggers={["click"]}
+                  onRoll={(rolled) => this.onDiceClick(rolled, 2)}
+                  disabled={isSecondClicked}
+                />
+              </div>
+            </div>
+            <div className={classes.endTurn}>
+              <Button onClick={() => this.endTurnHandler()}>End Turn</Button>
+            </div>
+            <div className={classes.score}>
+              {!isFirstClicked && !isSecondClicked && !isBotTurn
+                ? lastBotSum
+                : currentSum}
+            </div>
+          </div>
+          <div className={classes.buttonMenu}>
+            <Button onClick={this.newGame}>New game</Button>
+            <Button
+              onClick={() => {
+                this.setState({
+                  isRules: true,
+                });
+              }}
+            >
+              Rules
+            </Button>
+          </div>
+          <div className={classes.UsersButton}>
+            <input type="radio" id="1" name="mul" />
+            <input type="radio" id="2" name="mul" />
+            <input type="radio" id="3" name="mul" />
+            <input type="radio" id="4" name="mul" />
+            <input type="radio" id="plus" name="finalOperation" />
+            <input type="radio" id="minus" name="finalOperation" />
+            <div className={classes.row}>
+              <label for="1" onClick={() => this.setState({ mul: 1 })}>
+                x1
+              </label>
+              <label for="2" onClick={() => this.setState({ mul: 2 })}>
+                x2
+              </label>
+            </div>
+            <div className={classes.row}>
+              <label for="3" onClick={() => this.setState({ mul: 3 })}>
+                x3
+              </label>
+              <label for="4" onClick={() => this.setState({ mul: 4 })}>
+                x4
+              </label>
+            </div>
+            <div className={classes.row}>
+              <label for="plus" onClick={() => this.setState({ koef: 1 })}>
+                +
+              </label>
+              <label for="minus" onClick={() => this.setState({ koef: -1 })}>
+                -
+              </label>
+            </div>
+          </div>
+          <div className={classes.activityLabel}>
+            <div>Your activity</div>
+            {userLogs.map(element => <p>{element}</p>)}
+            <div className={classes.scoreWrapper}>User Score: {userSum}</div>
+          </div>
+          {isRules ? (
+            <Popup>
+              <Message onClose={() => this.setState({ isRules: false })}>
+                <p>
+                  Эта игра производится не совсем традиционными кубиками. Для
+                  нее один зар следует обработать так, чтобы на его гранях
+                  значились очки от двух до семи. Другой зар остается обычным,
+                  без изменений. Этими двумя кубиками и ведется игра. Кости
+                  помещаются в стаканчик и выбрасываются из него поочередно пять
+                  раз подряд в каждом круге. Количество игроков не
+                  ограничивается. Цель игры — набрать наибольшее количество
+                  очков, кратное 13
+                </p>
+                <p>
+                  Сумму очков, полученную при первом и последующих бросках,
+                  разрешается прибавлять к предыдущей сумме без изменений
+                  (объявляется плюс или складываю) или вычитать из нее
+                  (объявляется минус или вычитаю). Можно также умножать на два,
+                  три и четыре и прибавлять результат к предыдущей сумме (множу
+                  на два, три, четыре...) или делить на те же числа (беру
+                  половину, треть, четверть...).
+                </p>
+              </Message>
+            </Popup>
+          ) : null}
+          {errorEndTurn ? (
+            <Popup>
+              <Message onClose={() => this.setState({ errorEndTurn: false })}>
+                <p style={{fontSize: "2rem", margin: "0px"}}>Throw Dice!!!!</p>
+              </Message>
+            </Popup>
+          ) : null}
+          {isNewGame ? (
+            <Popup>
+              <Message onClose={this.newGame}>
+                <p style={{textAlign: "center", fontSize: "2.5rem", margin: "0px"}}>Game Over</p>
+                <p style={{fontSize: "2rem", marginRight: "400px"}}>Your score: {userSum}</p>
+                <p style={{fontSize: "2rem", marginRight: "400px"}}>Bots score: {botsSum}</p>
+              </Message>
+            </Popup>
+          ) : null}
+        </main>
+      </>
+    );
+  }
+}
+
+export default Layout;
